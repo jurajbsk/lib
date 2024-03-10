84802024ada4a26bf66b6aea22623e08641cf014
jurajbsk <104356522+jurajbsk@users.noreply.github.com>
2024-03-10 21:09:15 +0100

    @safe proofed

--- src/lib/sys/windows/advapi32.d
+++ src/lib/sys/windows/advapi32.d
@@ -3,5 +3,5 @@ import lib.sys.windows.kernel32;
 
 mixin dynamicLoad!("advapi32.dll", __MODULE__);
 
-extern(Windows) nothrow __gshared:
+extern(Windows) @safe nothrow:
 bool function(char* stringBuffer, int* sizeBuffer) GetUserNameA;

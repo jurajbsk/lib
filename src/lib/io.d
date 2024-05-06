59959e26f225d0326cbea7aff54a1bb1bcf570fe
jurajbsk <104356522+jurajbsk@users.noreply.github.com>
2024-05-06 18:46:21 +0200

    write()

--- src/lib/io.d
+++ src/lib/io.d
@@ -35,7 +35,7 @@ File stderr() {
 	static _0Std!2 file;
 	return file;
 }
-void write(File file, string msg)
+void writeA(File file, string msg)
 {
 	version(Windows) {
 		import lib.sys.windows.kernel32;
@@ -48,4 +48,56 @@ void writeW(File file, wstring msg)
 		import lib.sys.windows.kernel32;
 		WriteConsoleW(file, msg.ptr, cast(uint)msg.length, null, null);
 	}
+}
+
+import lib.string;
+void fwrite(S...)(File outFile, S args)
+{
+	void writeOut(string msg) => writeA(outFile, msg);
+
+	static foreach(arg; args) {{
+		alias argType = typeof(arg);
+		static if(is(typeof(arg) == U*, U)) {
+			if(arg) {
+			fwrite(outFile, *arg);
+			}
+			else writeOut("null");
+		}
+		else static if(is(immutable(argType) : wstring)) {
+			writeW(stdout, arg);
+		}
+		else static if(is(immutable(argType) : immutable(char))) {
+			writeOut(cast(string)(&arg)[0..1]);
+		}
+		else static if(is(argType == struct)) {
+			string header = __traits(identifier, argType) ~ "(";
+			writeOut(header);
+			enum string[] members = [__traits(allMembers, argType)];
+			static foreach(i, membStr; members) {{
+				auto member = __traits(getMember, arg, membStr);
+				if(is(typeof(member) == return) || is(typeof(member) == typeof(arg))) {
+					// member is func or __ctor!
+				}
+				else {
+					fwrite(outFile, member);
+					if(i+1 < members.length-1) {
+						writeOut(", ");
+					}
+				}
+			}}
+			writeOut(")");
+		}
+		else {
+			char[64] buf;
+			writeOut(toString(arg, buf));
+		}
+	}}
+}
+
+void write(S...)(S args) => fwrite(stdout, args);
+void writeln(S...)(S args) => write(args, '\n');
+
+unittest
+{
+	writeln("「ただいま印刷中」! 😃 "w, 51_235);
 }
